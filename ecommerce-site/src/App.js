9ae34ea2dfe87dcbe67e5502e969e12430bc5b88
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2021-08-06 17:57:41 +0530

    add products page to app

--- ecommerce-site/src/App.js
+++ ecommerce-site/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { NavigationBar } from './components/NavigationBar';
 import { Home } from './components/Home';
+import { ProductClothing } from './components/ProductClothing';
+import { ProductAssesories } from './components/ProductAssesories';
+import { ProductElectronic } from './components/ProductElectronic';
 function App() {
   return (
     <Router>
@@ -11,8 +14,14 @@ function App() {
         <Route exact path="/">
           <Home/>
         </Route>
-        <Route exact path="/product">
-          <Home/>
+        <Route exact path="/product/clothing">
+         <ProductClothing/>
+        </Route>
+        <Route exact path="/product/assesories">
+         <ProductAssesories/>
+        </Route>
+        <Route exact path="/product/electronics">
+         <ProductElectronic/>
         </Route>
       </Switch>
     </Router>
