675de83380ac2bae6cc8252ea50eb9f1051a7e66
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2021-08-11 00:54:34 +0530

    Update Dashboard.jsx

--- ecommerce-site/src/components/Dashboard.jsx
+++ ecommerce-site/src/components/Dashboard.jsx
@@ -11,79 +11,12 @@ const Dashboard = () => {
     var sale = data.filter((item) => item.sale === "true")
     return (
         <>
-            <div className="text-center text-danger my-4">
-                <h1>Welcome To Admin Panel</h1>
-            </div>
             <Container>
-                <div className="row my-4">
-                    <div className="col-md-6 text-center p-4">
-                        <Link to="/dashboard/total-products" className="text-decoration-none">
-                            <Card className="shadow">
-                                <Card.Title>
-                                    <h2 className="text-primary my-4">
-                                        Total Product
-                                    </h2>
-                                    <h3 className="text-success mb-4">{data.length}</h3>
-                                </Card.Title>
-                            </Card>
-                        </Link>
-                    </div>
-                    <div className="col-md-6 text-center p-4">
-                        <Link to="/product/sale" className="text-decoration-none">
-                            <Card className="shadow">
-                                <Card.Title>
-                                    <h2 className="text-primary my-4">
-                                        Sale Product
-                                    </h2>
-                                    <h3 className="text-success mb-4">{sale.length}</h3>
-                                </Card.Title>
-                            </Card>
-                        </Link>
-                    </div>
-                </div>
-                <div className="row my-4 mb-4">
-                    <div className="col-md-4 text-center p-4">
-                        <Link to="/product/clothing" className="text-decoration-none">
-                            <Card className="shadow">
-                                <Card.Title>
-                                    <h2 className="text-primary my-4">
-                                        Clothing Product
-                                    </h2>
-                                    <h3 className="text-success mb-4">{clothing.length}</h3>
-                                </Card.Title>
-                            </Card>
-                        </Link>
-                    </div>
-                    <div className="col-md-4 text-center p-4">
-                        <Link to="/product/electronics" className="text-decoration-none">
-                            <Card className="shadow">
-                                <Card.Title>
-                                    <h2 className="text-primary my-4">
-                                        Electronics Product
-                                    </h2>
-                                    <h3 className="text-success mb-4">{electronic.length}</h3>
-                                </Card.Title>
-                            </Card>
-                        </Link>
-                    </div>
-                    <div className="col-md-4 text-center p-4">
-                        <Link to="/product/assesories" className="text-decoration-none">
-                            <Card className="shadow">
-                                <Card.Title>
-                                    <h2 className="text-primary my-4">
-                                        Assesories Product
-                                    </h2>
-                                    <h3 className="text-success mb-4">{assesories.length}</h3>
-                                </Card.Title>
-                            </Card>
-                        </Link>
-                    </div>
-                </div>
-                <div className="text-center text-danger my-4">
+            <div className="text-center text-danger my-4">
                     <h1>Graphical View Of Products</h1>
                 </div>
                 <div className="text-center">
-                <Card className="shadow" style={{height:610, width:1010, marginLeft:150}}>
+                <Card className="shadow" style={{height:500, width:1010, marginLeft:100}}>
                     <Chart
                         width={'800px'}
                         height={'100px'}
@@ -102,13 +35,13 @@ const Dashboard = () => {
                                 },
                             ],
                             ['Clothing Products', parseInt(`${clothing.length}`), 'blue', null],
-                            ['Electronics Products  ', `${electronic.length}`, 'yellow', null],
-                            ['Assesories Products  ', `${assesories.length}`, 'green', null],
+                            ['Electronics Products  ', `${electronic.length}`, 'blue', null],
+                            ['Assesories Products  ', `${assesories.length}`, 'blue', null],
                         ]}
                         options={{
                             title: 'Item in Category',
                             width: 1000,
-                            height: 600,
+                            height: 500,
                             bar: { groupWidth: '50%' },
                             legend: { position: 'none' },
                         }}
@@ -117,6 +50,71 @@ const Dashboard = () => {
                     />
                 </Card>
                 </div>
+                <div className="row my-4">
+                    <div className="col-md-6 text-center">
+                        <Link to="/dashboard/total-products" className="text-decoration-none">
+                            <Card className="shadow">
+                                <Card.Title>
+                                    <h2 className="text-primary my-4">
+                                        Total Product
+                                    </h2>
+                                    <h3 className="text-success mb-4">{data.length}</h3>
+                                </Card.Title>
+                            </Card>
+                        </Link>
+                    </div>
+                    <div className="col-md-6 text-center">
+                        <Link to="/product/sale" className="text-decoration-none">
+                            <Card className="shadow">
+                                <Card.Title>
+                                    <h2 className="text-primary my-4">
+                                        Sale Product
+                                    </h2>
+                                    <h3 className="text-success mb-4">{sale.length}</h3>
+                                </Card.Title>
+                            </Card>
+                        </Link>
+                    </div>
+                </div>
+                <div className="row my-4 mb-4">
+                    <div className="col-md-4 text-center">
+                        <Link to="/product/clothing" className="text-decoration-none">
+                            <Card className="shadow">
+                                <Card.Title>
+                                    <h2 className="text-primary my-4">
+                                        Clothing Product
+                                    </h2>
+                                    <h3 className="text-success mb-4">{clothing.length}</h3>
+                                </Card.Title>
+                            </Card>
+                        </Link>
+                    </div>
+                    <div className="col-md-4 text-center">
+                        <Link to="/product/electronics" className="text-decoration-none">
+                            <Card className="shadow">
+                                <Card.Title>
+                                    <h2 className="text-primary my-4">
+                                        Electronics Product
+                                    </h2>
+                                    <h3 className="text-success mb-4">{electronic.length}</h3>
+                                </Card.Title>
+                            </Card>
+                        </Link>
+                    </div>
+                    <div className="col-md-4 text-center">
+                        <Link to="/product/assesories" className="text-decoration-none">
+                            <Card className="shadow">
+                                <Card.Title>
+                                    <h2 className="text-primary my-4">
+                                        Assesories Product
+                                    </h2>
+                                    <h3 className="text-success mb-4">{assesories.length}</h3>
+                                </Card.Title>
+                            </Card>
+                        </Link>
+                    </div>
+                </div>
+                
             </Container>
         </>
     )
